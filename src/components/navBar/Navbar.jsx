61ab21e18d3b3c041c69c51b5d3c7367e6c3ffaf
Yanir <yanir401@gmail.com>
2022-06-03 18:45:47 +0300

    added layout and api calls

--- src/components/navBar/Navbar.jsx
+++ src/components/navBar/Navbar.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./navbar.css";
+import { RiMovie2Fill } from "react-icons/ri";
 const Navbar = () => {
   return (
-    <h2>
-      <span className="white-letter">G</span>et
-      <span>M</span>
-      ovie
-    </h2>
+    <div className="logo">
+      <RiMovie2Fill size="2rem" />
+      <h2>GETMOVIE</h2>
+    </div>
   );
 };
 
